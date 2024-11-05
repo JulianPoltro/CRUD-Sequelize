fc69b6d284df2e91f53f4bebc5f7a51ee2bcd277
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2024-11-05 13:01:14 -0300

    port

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.get("/", (req, res) => {
 
 // Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
