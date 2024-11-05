a8b14d00aa03fc3681ccb5177135c4c0ed191ef9
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2024-11-05 12:07:05 -0300

    railway

--- conexion/database.js
+++ conexion/database.js
@@ -1,12 +1,19 @@
-require("dotenv").config();
 const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+const ENV = process.env.NODE_ENV || 'local'
+dotenv.config({ path: `.env.${ENV}` })
 
 // process.loadEnvFile();
-const { DBUSER, PASSWORD, HOST, DATABASE } = process.env;
+// const { DBUSER, PASSWORD, HOST, DATABASE } = process.env;
 
-const sequelize = new Sequelize(DATABASE, DBUSER, PASSWORD, {
-  host: HOST,
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DBUSER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    dialect: "mysql",
+  });
 
 module.exports = { sequelize };
